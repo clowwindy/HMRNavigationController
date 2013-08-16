d6a4cdf4833aff4a21f9ef109eaf7bb245a13c3f
clowwindy <clowwindy42@gmail.com>
2013-08-16 13:43:06 +0800

    Initial Commit

---
+++ HMRNavigationController/HMRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HMRAppDelegate.h
+//  HMRNavigationController
+//
+//  Created by clowwindy on 8/16/13.
+//  Copyright (c) 2013 zhihu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HMRNavigationController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HMRNavigationController
+//
+//  Created by clowwindy on 8/16/13.
+//  Copyright (c) 2013 zhihu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HMRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HMRAppDelegate class]));
+    }
+}

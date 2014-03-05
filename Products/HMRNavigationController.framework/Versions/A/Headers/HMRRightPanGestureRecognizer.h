7905bc16727231b33d20452741fb59c72b397be4
clowwindy <clowwindy42@gmail.com>
2014-03-05 19:18:46 +0800

    update

---
+++ Products/HMRNavigationController.framework/Versions/A/Headers/HMRRightPanGestureRecognizer.h
@@ -0,0 +1,13 @@
+//
+//  HMRRightPanGestureRecognizer.h
+//  HMRNavigationController
+//
+//  Created by clowwindy on 13-12-27.
+//  Copyright (c) 2013年 zhihu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMRRightPanGestureRecognizer : UIPanGestureRecognizer
+
+@end
